f82f9cfd64142d9da4c0ddac90dfd661e6aeec85
manya-bansal1302 <manya1262.becse24@chitkara.edu.in>
2025-02-02 19:12:54 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -10,7 +10,7 @@ int main() {
     {
         printf("Vowel");
     }
-    else if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch<='Z')){
+    else if((ch >= 'a' && ch <= 'z') && (ch >= 'A' && ch<='Z')){
         printf("Consonant");
     }
     else if(ch >= '0' && ch<='9'){
